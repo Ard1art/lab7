40fd56d45d58b69c9649dd1c5d71406228d60d31
Ard1art <32478235+Ard1art@users.noreply.github.com>
2020-12-23 10:00:36 +0200

    Update lab7.tf

--- lab7.tf
+++ lab7.tf
@@ -1,116 +1,138 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 2.70"
-    }
-  }
-}
-
 provider "aws" {
-  profile = "default"
-  region  = "us-east-1"
+  region = "us-east-1"
 }
 
-# base initialization
-resource "aws_vpc" "primary_vpc" {
-    cidr_block = "7.0.0.0/16"
-    enable_dns_support = true
-    enable_dns_hostnames = true
-
-    tags = {
-        Name = "forLab7"
-    }
-}
-
-resource "aws_subnet" "subnet_1" {
-    vpc_id = aws_vpc.primary_vpc.id
-    cidr_block = "7.0.0.0/24"
-    map_public_ip_on_launch = true
-    availability_zone = "us-east-1a"
-    tags = {
-        Name = "forLab7_1" 
-    }
-}
-
-resource "aws_subnet" "subnet_2" {
-    vpc_id = aws_vpc.primary_vpc.id
-    cidr_block = "7.0.1.0/24"
-    map_public_ip_on_launch = true
-    availability_zone = "us-east-1b"
-    tags = {
-        Name = "forLab7_2" 
-    }
-}
-# internet access
-resource "aws_internet_gateway" "gw" {
-    vpc_id = aws_vpc.primary_vpc.id
-}
-# route table
-resource "aws_route_table" "rtb" {
-    vpc_id = aws_vpc.primary_vpc.id
-    
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.gw.id
-
-    }
-}
-
-# assing rtb -> subnets
-resource "aws_route_table_association" "subnet_1_asoc" {
-    route_table_id = aws_route_table.rtb.id
-    subnet_id = aws_subnet.subnet_1.id
-}
-
-
-resource "aws_route_table_association" "subnet_2_asoc" {
-    route_table_id = aws_route_table.rtb.id
-    subnet_id = aws_subnet.subnet_1.id
-}
-
-# security group for database
-resource "aws_security_group" "sg" {
-    name = "forLab7"
-    vpc_id = aws_vpc.primary_vpc.id
-    
-    ingress {
-        from_port = 3306
-        to_port = 3306
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = -1
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-}
-
-# special subnet group (for db required!!)
-resource "aws_db_subnet_group" "subnet_group" {
-  name = "for-lab7"
-  subnet_ids = [ aws_subnet.subnet_1.id, aws_subnet.subnet_2.id ]
+resource "aws_vpc" "my_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  enable_dns_hostnames = true
 
   tags = {
-    Name = "forLab7"
+    Name = "RDS VPC"
   }
 }
 
-# db instance
-resource "aws_db_instance" "db" {
-  publicly_accessible = true
-  vpc_security_group_ids = [aws_security_group.sg.id]
-  db_subnet_group_name = aws_db_subnet_group.subnet_group.id
-  allocated_storage = 5
-  storage_type = "gp2"
-  engine = "mysql"
-  engine_version = "5.7"
-  instance_class = "db.t2.micro"
-  name = "forlabseven"
-  username = "testuser"
-  password = "Lgfd!53Kjst34"
-  parameter_group_name = "default.mysql5.7"
+#BLOCK IP
+resource "aws_default_network_acl" "default" {
+  default_network_acl_id = aws_vpc.my_vpc.default_network_acl_id
+
+  ingress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = aws_vpc.my_vpc.cidr_block
+    from_port  = 0
+    to_port    = 0
+  }
+  ingress {
+    protocol   = -1
+    rule_no    = 101
+    action     = "deny"
+    cidr_block = "50.31.252.0/24"
+    from_port  = 0
+    to_port    = 0
+  }
+
+  egress {
+    protocol   = -1
+    rule_no    = 100
+    action     = "allow"
+    cidr_block = "0.0.0.0/0"
+    from_port  = 0
+    to_port    = 0
+  }
+}
+
+resource "aws_subnet" "rds_public_us_east_1a" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "Public Subnet us-east-1a"
+  }
+}
+
+resource "aws_subnet" "rds_public_us_east_1b" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name = "Public Subnet us-east-1b"
+  }
+}
+
+resource "aws_internet_gateway" "my_vpc_igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "My VPC - Internet Gateway"
+  }
+}
+
+resource "aws_route_table" "my_vpc_public" {
+    vpc_id = aws_vpc.my_vpc.id
+
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.my_vpc_igw.id
+    }
+
+    tags = {
+        Name = "Public Subnets Route Table for My VPC"
+    }
+}
+
+resource "aws_route_table_association" "my_vpc_us_east_1a_public" {
+    subnet_id = aws_subnet.rds_public_us_east_1a.id
+    route_table_id = aws_route_table.my_vpc_public.id
+}
+
+resource "aws_route_table_association" "my_vpc_us_east_1b_public" {
+    subnet_id = aws_subnet.rds_public_us_east_1b.id
+    route_table_id = aws_route_table.my_vpc_public.id
+}
+
+
+
+resource "aws_db_subnet_group" "rds-private-subnet" {
+  name       = "rds-private-subnet-group"
+  subnet_ids = ["${aws_subnet.rds_public_us_east_1a.id}", "${aws_subnet.rds_public_us_east_1b.id}"]
+}
+
+resource "aws_security_group" "rds-sg" {
+  name   = "my-rds-sg"
+  vpc_id = "${aws_vpc.my_vpc.id}"
+}
+
+# Ingress Security Port 3306
+resource "aws_security_group_rule" "mysql_inbound_access" {
+  from_port         = 3306
+  protocol          = "tcp"
+  security_group_id = "${aws_security_group.rds-sg.id}"
+  to_port           = 3306
+  type              = "ingress"
+  cidr_blocks       = ["0.0.0.0/0"]
+}
+
+resource "aws_db_instance" "my_test_mysql" {
+  allocated_storage           = 20
+  storage_type                = "gp2"
+  engine                      = "mysql"
+  engine_version              = "5.7"
+  instance_class              = "db.t2.micro"
+  publicly_accessible         = true
+  name                        = "dbtest"
+  username                    = "testuser"
+  password                    = "Lgfd!53Kjst34"
+  parameter_group_name        = "default.mysql5.7"
+  db_subnet_group_name        = "${aws_db_subnet_group.rds-private-subnet.name}"
+  vpc_security_group_ids      = ["${aws_security_group.rds-sg.id}"]
+  allow_major_version_upgrade = true
+  auto_minor_version_upgrade  = true
+  backup_retention_period     = 35
+  backup_window               = "22:00-23:00"
+  maintenance_window          = "Sat:00:00-Sat:03:00"
+  multi_az                    = true
+  skip_final_snapshot         = true
 }
